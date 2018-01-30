508d556b3a62af4fef6923e154acfd516a6af018
= <tquerubim@gmail.com>
2018-01-29 23:40:35 -0200

    Add initial files

---
+++ jsf-livraria/src/br/com/livraria/bean/LivroBean.java
@@ -0,0 +1,75 @@
+package br.com.livraria.bean;
+
+import java.util.List;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
+
+import br.com.livraria.dao.DAO;
+import br.com.livraria.modelo.Autor;
+import br.com.livraria.modelo.Livro;
+
+@ManagedBean
+@ViewScoped
+public class LivroBean {
+
+	private Livro livro = new Livro();
+	private Integer autorId;
+
+	public Livro getLivro() {
+		return livro;
+	}
+
+	public void gravar() {
+		System.out.println("Gravando livro " + this.livro.getTitulo());
+
+		if (livro.getAutores().isEmpty()) {
+			FacesContext.getCurrentInstance().addMessage("autor",
+					new FacesMessage("Livro deve ter pelo menos um Autor"));
+		}
+
+		new DAO<Livro>(Livro.class).adiciona(this.livro);
+		this.livro = new Livro();
+	}
+
+	public void gravarAutor() {
+		Autor autor = new DAO<Autor>(Autor.class).buscaPorId(autorId);
+		this.livro.adicionaAutor(autor);
+	}
+
+	public List<Autor> getAutores() {
+		return new DAO<Autor>(Autor.class).listaTodos();
+	}
+
+	public List<Autor> getAutoresDoLivro() {
+		return this.livro.getAutores();
+	}
+
+	public Integer getAutorId() {
+		return autorId;
+	}
+
+	public void setAutorId(Integer autorId) {
+		this.autorId = autorId;
+	}
+
+	public void setLivro(Livro livro) {
+		this.livro = livro;
+	}
+
+	public void comecaComDigitoUm(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+
+		String valor = value.toString();
+		if (!valor.startsWith("1")) {
+			throw new ValidatorException(new FacesMessage("Deveria começar com 1"));
+		}
+	}
+	
+	public List<Livro> getLivros(){
+		return new DAO<Livro>(Livro.class).listaTodos();
+	}
+}
